0c90c425a2d70ae1f6457158468e8e29f9b64b37
Simon W. Jackson <simon@simonwjackson.com>
2016-01-20 15:16:46 +0100

    small updates

--- humid.js
+++ humid.js
@@ -2,11 +2,11 @@
 
 var util = require("util");
 
-var fs      = require('fs'),
-    path    = require('path'),
-    wrench  = require('wrench'),
+var fs = require('fs'),
+    path = require('path'),
+    wrench = require('wrench'),
     Promise = require('bluebird'),
-    mv      = require('mv');
+    mv = require('mv');
 
 Promise.promisifyAll(fs);
 
@@ -15,114 +15,118 @@ Promise.promisifyAll(fs);
 ///////////
 
 function removeDoubleQuotes(str) {
-	return str.replace(/"/gi, '');
+    return str.replace(/"/gi, '');
 }
 
 function getContent(filename) {
-	return fs.readFileAsync(filename);
+    return fs.readFileAsync(filename);
 }
 
 function parseManifestLine(line, library_path) {
-  var steamApp = [];
-  var entry = line.trim().split("\t");
-  entry[0] = removeDoubleQuotes(entry[0].toLowerCase());
-  entry = entry.filter(function(n){ return n != '' });
+    var steamApp = [];
+    var entry = line.trim().split("\t");
+    entry[0] = removeDoubleQuotes(entry[0].toLowerCase());
+    entry = entry.filter(function(n) {
+        return n != ''
+    });
 
-  if (entry[0] == "appid") {
-    steamApp['appid'] = removeDoubleQuotes(entry[1]);
-  }
-  else if (entry[0] == "name") {
-    steamApp['name'] = removeDoubleQuotes(entry[1]);
-  }
-  else if (entry[0] == "sizeondisk") {
-    steamApp['size'] = removeDoubleQuotes(entry[1]);
-  }
-  else if (entry[0] == "installdir") {
-    steamApp['dir_name'] = removeDoubleQuotes(entry[1]);
-    steamApp['full_path'] = path.join(library_path, 'steamapps', 'common', steamApp['dir_name']);
-  }
+    if (entry[0] == "appid") {
+        steamApp['appid'] = removeDoubleQuotes(entry[1]);
+    } else if (entry[0] == "name") {
+        steamApp['name'] = removeDoubleQuotes(entry[1]);
+    } else if (entry[0] == "sizeondisk") {
+        steamApp['size'] = parseInt(removeDoubleQuotes(entry[1]));
+    } else if (entry[0] == "installdir") {
+        steamApp['dir_name'] = removeDoubleQuotes(entry[1]);
+        steamApp['full_path'] = path.join(library_path, 'steamapps', 'common', steamApp['dir_name']);
+    }
 
-  return steamApp;
+    return steamApp;
 }
 
 function parseAcf(fullPath, library_path) {
-	var steamApp = {};
+    var steamApp = {};
 
-	return new Promise(function(resolve, reject) {
-		getContent(fullPath)
-			.then(function(content) {
-				content.toString().split('\n')
-					.forEach(function(line) {
-            var new_line = parseManifestLine(line, library_path);
-            steamApp = Object.assign(steamApp, new_line);
-					});
+    return new Promise(function(resolve, reject) {
+        getContent(fullPath)
+            .then(function(content) {
+                content.toString().split('\n')
+                    .forEach(function(line) {
+                        var new_line = parseManifestLine(line, library_path);
+                        steamApp = Object.assign(steamApp, new_line);
+                    });
 
-				steamApp['manifest'] = fullPath;
-			})
-			.finally(function(){
-				resolve(steamApp);
-			});
-	});
+                steamApp['manifest'] = fullPath;
+            })
+            .finally(function() {
+                resolve(steamApp);
+            });
+    });
 }
 
 function getManifestFiles(library_path) {
-	return new Promise(function(resolve, reject) {
-		var acf_path = path.join(library_path, 'steamapps');
-		var app_path = path.join(acf_path, 'common');
-		var pth = fs.readdirAsync(acf_path);
+    return new Promise(function(resolve, reject) {
+        var acf_path = path.join(library_path, 'steamapps');
+        var app_path = path.join(acf_path, 'common');
+        var pth = fs.readdirAsync(acf_path);
 
-		Promise.map(pth, function(file, index, length) {
-			var fullpath = path.join(acf_path, file);
-			var isFile = fs.statSync(fullpath).isFile();
+        Promise.map(pth, function(file, index, length) {
+            var fullpath = path.join(acf_path, file);
+            var isFile = fs.statSync(fullpath).isFile();
 
-			if (isFile) {
-				var ext =	path.extname(fullpath);
+            if (isFile) {
+                var ext = path.extname(fullpath);
 
-				if (ext == '.acf') {
-					return Promise.resolve(file);
-				}
-			}
-		}).then(function(arr){
-			resolve(arr.filter(function(n){
-				return n != undefined
-			}));
-		});
-	});
+                if (ext == '.acf') {
+                    return Promise.resolve(file);
+                }
+            }
+        }).then(function(arr) {
+            resolve(arr.filter(function(n) {
+                return n != undefined
+            }));
+        });
+    });
 }
 
 ////////////
 // Exports
 ///////////
 
-exports.moveSteamApp = function(app, dest, cb) {
-	var app_final_path = path.join(dest, 'steamapps', 'common', app['dir_name'])
-	var manifest_final_path = path.join(dest, 'steamapps', 'appmanifest_' + app['appid'] + '.acf')
+var humid = {
+    find: function(libraries) {
+        var steamApps = [];
 
-	console.log('Moving..', app['name']);
+        return new Promise(function(resolve, reject) {
+            Promise.each(libraries, function(library_path) {
+                    return getManifestFiles(library_path).then(function(manifest_files) {
+                        return Promise.mapSeries(manifest_files, function(file) {
+                                var acf_path = path.join(library_path, 'steamapps');
+                                var fullpath = path.join(acf_path, file);
+                                return parseAcf(fullpath, library_path);
+                            })
+                            .then(function(data) {
+                                steamApps = steamApps.concat(data);
+                            });
+                    })
+                })
+                .then(function() {
+                    resolve(steamApps);
+                });
+        })
+    },
+    move: function(app, dest, cb) {
+        var app_final_path = path.join(dest, 'steamapps', 'common', app['dir_name'])
+        var manifest_final_path = path.join(dest, 'steamapps', 'appmanifest_' + app['appid'] + '.acf')
 
-	mv(app['manifest'], manifest_final_path, {mkdirp: true});
-	wrench.copyDirSyncRecursive(app['full_path'], app_final_path);
-	cb('done');
+        console.log('Moving..', app['name']);
+
+        mv(app['manifest'], manifest_final_path, {
+            mkdirp: true
+        });
+        wrench.copyDirSyncRecursive(app['full_path'], app_final_path);
+        cb('done');
+    },
 };
 
-exports.getSteamApps = function(libraries) {
-	var steamApps = [];
-
-	return new Promise(function(resolve, reject) {
-		Promise.each(libraries, function(library_path) {
-			return getManifestFiles(library_path).then(function(manifest_files) {
-			   return Promise.mapSeries(manifest_files, function(file) {
-				    var acf_path = path.join(library_path, 'steamapps');
-				    var fullpath = path.join(acf_path, file);
-				    return parseAcf(fullpath, library_path);
-			   })
-         .then(function(data){
-            steamApps = steamApps.concat(data);
-         });
-		  })
-		})
-    .then(function(){
-      resolve(steamApps);
-    });
-	});
-};
+module.exports = Object.create(humid);
--- test.js
+++ test.js
@@ -1,12 +1,70 @@
-var steam = require('./steam-local');
+var steam = require('./humid');
+var _ = require('lodash');
+var fs = require('fs');
+var Promise = require('bluebird');
+var exec = require('child_process').exec;
+var mv = Promise.promisify(require('mv'));
 
+var maxGB = 0;
+
+var moveTo = "h:";
 var libraries = [
-	'/run/media/simonwjackson/6C7E3EE008C5F5B5/games/binaries/steam-win',
-	'/media/storage/bowser/games/binaries/steam-win',
+    'g:\\games\\binaries\\steam-win',
 ];
 
+var reduceToSize = data => {
+    var init = {
+        items: [],
+        total: 0
+    };
+
+    data = data.reduce((memo, item) => {
+        memo.total += item.size;
+
+        if (memo.total <= maxGB)
+            memo.items.push(item);
+
+        return memo;
+    }, init);
+
+    return data.items;
+}
+
+var bytesFree = driveLetter => {
+    var command = 'fsutil volume diskfree ' + driveLetter;
+
+    return new Promise(function(res, rej) {
+        exec(command, function(error, stdout, stderr) {
+            var lines = stdout.split("\r\n");
+            maxGB = lines[0].replace("Total # of free bytes        : ", '');
+            res(maxGB);
+        });
+    });
+}
+
+var moveGame = item => {
+    var newGamePath = moveTo + item.full_path.substring(2);
+    var newManifestPath = moveTo + item.manifest.substring(2);
+    var opts = {
+        mkdirp: true
+    };
+
+    if (!fs.existsSync(item.full_path)) {
+        console.log(item.name, 'Missing.. Moving now..');
+
+        return mv(item.manifest, newManifestPath, opts);
+    } else {
+        console.log("Moving..", item.name);
+
+        return mv(item.full_path, newGamePath, opts)
+            .then(() => mv(item.manifest, newManifestPath, opts))
+    }
+}
+
 steam
-  .getSteamApps(libraries)
-	.then(function(data) {
-		console.log(data);
-	});
+    .find(libraries)
+    .tap(bytesFree.bind(null, moveTo))
+    .then(data => _.orderBy(data, ['size'], ['asc']))
+    .then(reduceToSize)
+    .mapSeries(moveGame)
+    .catch(console.log);
